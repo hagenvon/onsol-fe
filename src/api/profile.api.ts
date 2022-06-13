aa08e4415f3b1336802dd13f614fe4f6962cbbb6
hagenvon <31388529+hagenvon@users.noreply.github.com>
2022-06-13 15:39:17 +0200

    added wallet as parameter for loading profiles

--- src/api/profile.api.ts
+++ src/api/profile.api.ts
@@ -1,10 +1,13 @@
 import { sleep } from "../utils/sleep";
 import { createProfile, Profile } from "../types/Profile";
 
-export async function loadProfile(): Promise<Profile> {
+export async function loadProfile(wallet: string): Promise<Profile> {
   await sleep(1500);
 
-  return createProfile({ username: "Yo" });
+  return createProfile({
+    username: "Yo",
+    bio: wallet,
+  });
 }
 
 export async function saveProfile(profile: Profile): Promise<Profile> {
