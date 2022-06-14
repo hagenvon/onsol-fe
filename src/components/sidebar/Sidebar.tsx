4469f935bd64eb4c87eb002cf74b867abee831e1
hagenvon <31388529+hagenvon@users.noreply.github.com>
2022-06-14 17:56:48 +0200

    sidebar responsive basics

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,9 +1,10 @@
 import { SidebarContent } from "./SidebarContent";
+import { Box } from "@mantine/core";
 
 export function Sidebar() {
   return (
-    <div
-      style={{
+    <Box
+      sx={(theme) => ({
         position: "fixed",
         top: 0,
         bottom: 0,
@@ -11,17 +12,33 @@ export function Sidebar() {
         display: "flex",
         alignItems: "center",
         height: "100%",
-      }}
+
+        "@media (max-width: 992px)": {
+          right: 0,
+          zIndex: 100,
+        },
+      })}
     >
-      <div
-        style={{
+      <Box
+        sx={(theme) => ({
           width: "200px",
           height: 300,
           background: "white",
-        }}
+          borderTopRightRadius: "25px",
+          borderBottomRightRadius: "25px",
+          paddingTop: "25px",
+          paddingBottom: "25px",
+
+          "@media (max-width: 992px)": {
+            width: "100%",
+            height: "100%",
+            borderTopRightRadius: 0,
+            borderBottomRightRadius: 0,
+          },
+        })}
       >
         <SidebarContent />
-      </div>
-    </div>
+      </Box>
+    </Box>
   );
 }
--- src/components/sidebar/SidebarContentNav.tsx
+++ src/components/sidebar/SidebarContentNav.tsx
@@ -1,16 +1,40 @@
 import { useNavigate } from "react-router-dom";
 import { editSteps } from "../edit/edit.steps";
+import { UnstyledButton } from "@mantine/core";
 
 export function SidebarContentNav() {
   const navigate = useNavigate();
 
   const navItems = editSteps.map((it) => {
     return (
-      <div key={it.path} onClick={() => navigate(it.path)}>
+      <UnstyledButton
+        key={it.path}
+        onClick={() => navigate(it.path)}
+        sx={(theme) => ({
+          display: "block",
+          width: "100%",
+          padding: theme.spacing.xs,
+          borderRadius: theme.radius.sm,
+          color:
+            theme.colorScheme === "dark" ? theme.colors.dark[0] : theme.black,
+
+          "&:hover": {
+            backgroundColor:
+              theme.colorScheme === "dark"
+                ? theme.colors.dark[6]
+                : theme.colors.gray[0],
+          },
+        })}
+      >
         {it.linkLabel}
-      </div>
+      </UnstyledButton>
     );
   });
 
-  return <div>{navItems}</div>;
+  return (
+    <div>
+      <div>Edit Profile</div>
+      {navItems}
+    </div>
+  );
 }
